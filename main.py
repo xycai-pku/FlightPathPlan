e81fe91b77d1d598fc8db0d8152a5f8b15199b65
CXY <xycai@pku.edu.cn>
2021-10-06 00:26:01 +0800

    优化绘制,功能基本实现

--- main.py
+++ main.py
@@ -25,92 +25,98 @@ def pointTrans(point, M):
     temp_p = M.dot(homo_point)
     return np.array([temp_p[0],temp_p[1]])
 
-border = np.array([[0, 0], [-0.5, 0.1], [-1, 1.1], [0, 2], [0.5, 1.75], [1, 1], [0, 0]])
+#border = np.array([[0, 0], [-0.5, 0.1], [-1, 1.1], [0, 2], [0.5, 1.75], [1, 1], [0, 0]])
 
-px = 0.1
-py = 0.3
+def draw(border):
 
-point_index = 0  # 旋转边下标
-omega = 0  # 旋转角度
-min_width = 999999  # 凸多边形宽度
-point_set = []  # 变换后点集
-draw_point = []  # 绘制点集
-max_coord = []
-min_coord = []
-rotation_matrix = []  # 旋转矩阵
-trans_matrix = []  # 平移矩阵
-a_ = b_ = 0
+    border = np.array(border)
+    print(border)
+    px = 300
+    py = 200
 
-# 寻找最小宽度
-for i in range(0, len(border) - 1):
-    w = angle(border[i], border[i + 1])
-    a = border[i][0]
-    b = border[i][1]
-    temp = []
-    for p in border:
-        temp.append([(p[0] - a) * cos(w) + (p[1] - b) * sin(w), -(p[0] - a) * sin(w) + (p[1] - b) * cos(w)])
-    temp = np.array(temp)
-    max_c = temp.max(0)
-    min_c = temp.min(0)
-    # width = abs(temp.max(0)[1] - temp.min(0)[1])
-    width = abs(max_c[1] - min_c[1])
-    if width < min_width:
-        point_index = i
-        omega = w
-        min_width = width
-        point_set = temp
-        max_coord = max_c
-        min_coord = min_c
-        a_ = a
-        b_ = b
+    point_index = 0  # 旋转边下标
+    omega = 0  # 旋转角度
+    min_width = 9999999  # 凸多边形宽度
+    point_set = []  # 变换后点集
+    draw_point = []  # 绘制点集
+    max_coord = []
+    min_coord = []
+    rotation_matrix = []  # 旋转矩阵
+    trans_matrix = []  # 平移矩阵
+    a_ = b_ = 0
 
-rotation_matrix = np.array([[cos(omega), sin(omega), 0],
-                              [-sin(omega), cos(omega), 0],
-                              [0, 0, 1],
-                              ])
-trans_matrix = np.array([[1, 0, -a_],
-                           [0,1,-b_],
-                           [0,0,1]])
+    # 寻找最小宽度
+    for i in range(0, len(border) - 1):
+        w = angle(border[i], border[i + 1])
+        a = border[i][0]
+        b = border[i][1]
+        temp = []
+        for p in border:
+            temp.append([(p[0] - a) * cos(w) + (p[1] - b) * sin(w), -(p[0] - a) * sin(w) + (p[1] - b) * cos(w)])
+        temp = np.array(temp)
+        max_c = temp.max(0)
+        min_c = temp.min(0)
+        # width = abs(temp.max(0)[1] - temp.min(0)[1])
+        width = abs(max_c[1] - min_c[1])
+        if width < min_width:
+            point_index = i
+            omega = w
+            min_width = width
+            point_set = temp
+            max_coord = max_c
+            min_coord = min_c
+            a_ = a
+            b_ = b
 
+    rotation_matrix = np.array([[cos(omega), sin(omega), 0],
+                                [-sin(omega), cos(omega), 0],
+                                [0, 0, 1],
+                                ])
+    trans_matrix = np.array([[1, 0, -a_],
+                             [0, 1, -b_],
+                             [0, 0, 1]])
 
-draw_point.append(point_set[point_index])  # 起始点
-now_point = point_set[point_index]
-x_pos = y_pos = 1  # 方向
-if point_set[point_index + 1][0] < point_set[point_index][0]:
-    x_pos *= -1
-if min_coord[1] < 0:
-    y_pos *= -1
-
-# 扫描线算法
-while (True):
-    now_point = now_point + [x_pos * px, 0]
-    # 是否换成四个角,即用栅格相交
-
-    if cv2.pointPolygonTest(np.array(point_set * 100, np.int0), tuple(np.array(now_point * 100, np.int0)), False) >= 0 :
-        draw_point.append(now_point)
-
-    if now_point[0] > max_coord[0] or now_point[0] < min_coord[0]:  # x方向上越界
+    draw_point.append(point_set[point_index])  # 起始点
+    now_point = point_set[point_index]
+    x_pos = y_pos = 1  # 方向
+    if point_set[point_index + 1][0] < point_set[point_index][0]:
         x_pos *= -1
-        now_point = now_point + [0, y_pos * py]
-        if cv2.pointPolygonTest(np.array(point_set * 100, np.int32), tuple(np.array(now_point * 100, np.int32)),
+    if min_coord[1] < 0:
+        y_pos *= -1
+
+    # 扫描线算法
+    while (True):
+        now_point = now_point + [x_pos * px, 0]
+        # 是否换成四个角,即用栅格相交
+
+        if cv2.pointPolygonTest(np.array(point_set, np.int32), (int(now_point[0]),int(now_point[1])),
                                 False) >= 0:
             draw_point.append(now_point)
 
-    if abs(now_point[1]) > abs(min_coord[1]) and abs(now_point[0]) > max_coord[1]:
-        break
+        if now_point[0] > max_coord[0] or now_point[0] < min_coord[0]:  # x方向上越界
+            x_pos *= -1
+            now_point = now_point + [0, y_pos * py]
+            if cv2.pointPolygonTest(np.array(point_set, np.int32), (int(now_point[0]),int(now_point[1])),
+                                    False) >= 0:
+                draw_point.append(now_point)
 
-for i in range(0,len(point_set)):
-    point_set[i] = pointTrans(point_set[i],np.linalg.inv(trans_matrix).dot(np.linalg.inv(rotation_matrix)))
-for i in range(0,len(draw_point)):
-    draw_point[i] = pointTrans(draw_point[i],np.linalg.inv(trans_matrix).dot(np.linalg.inv(rotation_matrix)))
+        if abs(now_point[1]) > abs(min_coord[1]) and abs(now_point[0]) > max_coord[1]:
+            break
 
-def draw(points):
-    points = points.reshape((-1, 1, 2))
-    img = np.zeros((512, 512, 3), np.uint8)
-    cv2.polylines(img, [points], True, (0, 255, 255), 2)
-    for i in draw_point:
-        cv2.circle(img, center=(int(i[0] * 100 + 200), int(i[1] * 100 + 200)), radius=2, color=(0, 0, 255), thickness=1)
-    cv2.imwrite("test.jpg", img)
+    for i in range(0, len(point_set)):
+        point_set[i] = pointTrans(point_set[i], np.linalg.inv(trans_matrix).dot(np.linalg.inv(rotation_matrix)))
+    for i in range(0, len(draw_point)):
+        draw_point[i] = pointTrans(draw_point[i], np.linalg.inv(trans_matrix).dot(np.linalg.inv(rotation_matrix)))
 
+    return draw_point
 
-draw(np.array(point_set * 100 + (200, 200), np.int32))
+# def draw(points):
+#     points = points.reshape((-1, 1, 2))
+#     img = np.zeros((512, 512, 3), np.uint8)
+#     cv2.polylines(img, [points], True, (0, 255, 255), 2)
+#     for i in draw_point:
+#         cv2.circle(img, center=(int(i[0] * 100 + 200), int(i[1] * 100 + 200)), radius=2, color=(0, 0, 255), thickness=1)
+#     cv2.imwrite("test.jpg", img)
+#
+#
+# draw(np.array(point_set * 100 + (200, 200), np.int32))
--- map.py
+++ map.py
@@ -1,7 +1,15 @@
 import folium
 from flask import Flask,request,make_response
+import pyproj
+from main import draw
+import numpy as np
+import webbrowser
 
 app = Flask(__name__)
+
+proj = pyproj.Proj(proj = 'tmerc',lon_0 = 116,lat_0 = 40,preserve_units = False)
+points = []      #变换后边界点
+pre_points = []  #原边界点
 
 # m = folium.Map(location=[40,116],zoom_start=13,)
 # folium.Marker(
@@ -19,9 +27,13 @@ def resp():
     response.headers['Access-Control-Allow-Methods'] = 'GET'
     response.headers['Access-Control-Allow-Headers'] = 'x-requested-with,content-type'
     point = request.args.get('point').split(',')
-    lon = float(point[0])
-    lat = float(point[1])
-    print(lat,lon)
+    lat = float(point[0])
+    lng = float(point[1])
+
+    print(proj(lng,lat))
+    points.append(np.array(proj(lng,lat)))
+
+    pre_points.append([lat,lng])
     return  response
 
 @app.route('/draw')
@@ -31,8 +43,38 @@ def resp_draw():
     response.headers['Access-Control-Allow-Methods'] = 'GET'
     response.headers['Access-Control-Allow-Headers'] = 'x-requested-with,content-type'
 
-    print("begin draw")
-    return  response
+    draw_points = draw(points)
+    m = folium.Map(location=[40,116],zoom_start=13,)
+
+    for j in range(0,len(pre_points)-1):
+        folium.PolyLine(
+            locations=[[pre_points[j][0],pre_points[j][1]], [pre_points[j+1][0],pre_points[j+1][1]]],
+            icon=folium.Icon(color='red')
+        ).add_to(m)
+
+    folium.PolyLine(
+        locations=[[pre_points[0][0],pre_points[0][1]], [pre_points[len(pre_points)-1][0],pre_points[len(pre_points)-1][1]]],
+        icon=folium.Icon(color='red')
+    ).add_to(m)
+
+    for i in range(0,len(draw_points)):
+        reverse = proj(draw_points[i][0],draw_points[i][1],inverse = True)
+        folium.Marker(
+            location=[reverse[1],reverse[0] ],
+            icon=folium.Icon(color = 'red')
+        ).add_to(m)
+
+        if i > 1:
+            reverse_last = proj(draw_points[i-1][0],draw_points[i-1][1],inverse = True)
+            folium.PolyLine(
+                locations=[[reverse_last[1],reverse_last[0]],[reverse[1],reverse[0]]],
+                color='blue'
+            ).add_to(m)
+
+    m.save("test_draw.html")
+    webbrowser.open("test_draw.html")
+
+    return response
 
 if __name__ == '__main__':
     app.debug = True
